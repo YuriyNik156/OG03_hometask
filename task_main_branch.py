4cefc6bb734453481251ea3e3f6ba6726de45902
YuriyNik156 <yuriy.nik156@gmail.com>
2025-03-30 12:03:14 +0300

    Add find_number function
    Добавлена функция нахождения максимального числа

--- task_main_branch.py
+++ task_main_branch.py
@@ -15,4 +15,17 @@ try:
 except (ValueError, TypeError) as e:
     print(f"Ошибка: {e}")
 
+# Напишите функцию, которая принимает список чисел и возвращает наибольшее из них.
 
+def find_number(numbers):
+    if not numbers: # Проверяем, что список не пустой
+        raise ValueError("В списке должны быть числа")
+
+    max_number = numbers[0] # Начинаем с первого элемента в списке
+    for i in numbers:
+        if i > max_number:
+            max_number = i
+    return max_number
+
+numbers = [3, 7, 4, 11, 9, 6, 10]
+print(find_number(numbers))
